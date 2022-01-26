6c19dbbd045116d1510db41c0384a22038641804
Oussama BenHassine <ouben@alteva.fr>
2022-01-26 14:08:48 +0100

    added actors ,cinemas ,movies and  producers controller.

---
+++ eTickets/Controllers/ActorsController.cs
@@ -0,0 +1,26 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //https://localhost:44330/Actors(ControllerName)/Index
+        public async Task<IActionResult> Index()//ActionResult by default (Index)
+        {
+            var allActors = await  _context.Actors.ToListAsync(); //get the data
+            return View();
+        }
+    }
+}
